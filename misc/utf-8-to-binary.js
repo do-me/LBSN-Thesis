c8ea719c38687d81695de3ee7b5150290448ed0e
do-me <47481567+do-me@users.noreply.github.com>
2021-06-07 13:53:23 +0200

    Update utf-8-to-binary.js

--- misc/utf-8-to-binary.js
+++ misc/utf-8-to-binary.js
@@ -1,2 +1,3 @@
 "User123".split("").reduce(function (a, b) { return a + b.charCodeAt(0).toString(2)}, "")
 // Result: 1010101111001111001011110010110001110010110011
+// Alternative with duckduckgo: https://duckduckgo.com/?q=User123+to+binary
